093ab3a770223f3d50ffc9f46955502f12d1feb5
Olha Halaiba <olja@MacBookPro.lan>
2026-02-07 17:30:56 -0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: "/dashboard/", 
+   base: "/", 
   server: {
     host: "::",
     port: 8080,
